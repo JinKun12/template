9141087957529ed3c2c25ab81e8181a03516b40d
Spiriz <85022242+JinKun12@users.noreply.github.com>
2024-10-11 23:12:25 +0700

    Create dsu_rollback.cpp

---
+++ code/dsu_rollback.cpp
@@ -0,0 +1,40 @@
+struct DSU {
+    struct Data {
+        int a, b, sza, parb;
+    };
+    vector<Data> history;
+    int cnt;
+    int par[N], sz[N];
+    void make_set(int u) {
+        par[u] = u;
+        sz[u] = 1;
+    }
+    void init(int n) {
+        cnt = n;
+        for (int i = 1; i <= n; i++) make_set(i);
+        history.clear();
+    }
+    int Find(int u) {
+        return par[u] == u ? u : Find(par[u]);
+    }
+    bool Merge(int u, int v) {
+        u = Find(u);
+        v = Find(v);
+        if (u == v) return false;
+        if (sz[u] < sz[v]) swap(u, v);
+        history.pb({u, v, sz[u], par[v]});
+        par[v] = u;
+        sz[u] += sz[v];
+        --cnt;
+        return true;
+    }
+    void roll_back(int pos) {
+        while (sz(history) - 1 > pos) {
+            Data tmp = history.back();
+            history.pop_back();
+            par[tmp.b] = tmp.parb;
+            sz[tmp.a] = tmp.sza;
+            ++cnt;
+        }
+    }
+} dsu;
